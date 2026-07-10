327ff3402743d71653e48ab5e662d1c3094aab54
Tomasz Uściński <tomasz.uscinski@profitia.pl>
2026-07-10 09:55:57 +0200

    Revert "Add dashboard preview app for Render"
    This reverts commit d238f04f1c9155f71c58ad7ecf7b1663fd665f5a.

--- apps/dashboard-preview/components/component-search/index.tsx
+++
@@ -1,8 +0,0 @@
-export function ComponentSearch() {
-  return (
-    <div>
-      <strong>Component search</strong>
-      <p className="muted">Primary selector will use componentName, not the benchmark code.</p>
-    </div>
-  )
-}
