ac8bb4e0100b656df110afbf088ce7e5709e2fa2
Tomasz Uściński <tomasz.uscinski@profitia.pl>
2026-07-11 09:48:47 +0200

    Revert "Add dashboard preview app"
    This reverts commit 6429e030880494c57ac271b06f2373e6b5bcf8c0.

--- apps/dashboard-preview/components/records-table/index.tsx
+++
@@ -1,8 +0,0 @@
-export function RecordsTablePlaceholder() {
-  return (
-    <section className="panel" style={{ gridColumn: 'span 8' }}>
-      <strong>Records table placeholder</strong>
-      <p className="muted">Backing records used for the chart will be listed here.</p>
-    </section>
-  )
-}
--- apps/dashboard-preview/i18n/routing.ts
+++
@@ -1,8 +0,0 @@
-import { defineRouting } from 'next-intl/routing'
-
-export const routing = defineRouting({
-  locales: ['pl', 'en'],
-  defaultLocale: 'pl',
-})
-
-export type Locale = (typeof routing.locales)[number]
